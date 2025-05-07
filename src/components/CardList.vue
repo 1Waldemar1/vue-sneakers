a498df48b3c471e70b0d68a85d7ebb7084a685ee
1Waldemar1 <dantedmc.5@mail.ru>
2025-05-07 12:42:57 +0300

    first commit

---
+++ src/components/CardList.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Cart from './Cart.vue'
+</script>
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Cart imageUrl="/sneakers/sneakers-1.jpg" />
+    <Cart />
+    <Cart />
+    <Cart />
+    <Cart />
+    <Cart />
+    <Cart />
+  </div>
+</template>
---
+++ src/components/Cart.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  imageUrl: String,
+})
+</script>
+
+<template>
+  <div
+    class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition"
+  >
+    <img src="/like-1.svg" alt="Like 1" class="absolute top-8 left-8" />
+    <img :src="imageUrl" alt="Sneaker" />
+    <p class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <b>1205 руб.</b>
+      </div>
+
+      <img src="/plus.svg" alt="Plus" />
+    </div>
+  </div>
+</template>
